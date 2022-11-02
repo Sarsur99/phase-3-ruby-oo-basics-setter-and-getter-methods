f31d66e0b823ed9e622f12752fbe0b56d00afa82
Soren <sorenisme@gmail.com>
2022-11-02 11:33:09 -0400

    commit tree fe912f234d1b98d961be8d63d4397266214444e7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
+attr_reader :name, :breed
+attr_writer :name, :breed
+    
+    def initialize(name, breed)
+        @name = name
+        @breed = breed
+    end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
+attr_reader :name, :job
+attr_writer :name, :job
+    
+    def initialize(name, job)
+        @name = name
+        @job = job
+    end
 
 end
